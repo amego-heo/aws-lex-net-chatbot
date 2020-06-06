0bb5d6ca2dfe3fb19d4f8fe8686274e784c515fd
BachLX <heozoi@Leos-MacBook.local>
2020-06-06 15:46:41 +0900

    update script6

--- dotnetLexChatBot/Controllers/HomeController.cs
+++ dotnetLexChatBot/Controllers/HomeController.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using dotnetLexChatBot.Models;
 
 namespace dotnetLexChatBot.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
